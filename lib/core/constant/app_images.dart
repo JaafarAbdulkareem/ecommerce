4444a9b31c55be46011ff1b681651e8d3f1cea78
King <jafaar.abdulkareem@gmail.com>
2025-02-09 20:46:07 +0530

    add imges for onboarding and fonts family called cairo support arabic language

---
+++ lib/core/constant/app_images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  /// Assets for imagesOnboardingfour
+  /// assets/images/onboardingfour.PNG
+  static const String imagesOnboardingfour = "assets/images/onboardingfour.PNG";
+
+  /// Assets for imagesOnboardingone
+  /// assets/images/onboardingone.PNG
+  static const String imagesOnboardingone = "assets/images/onboardingone.PNG";
+
+  /// Assets for imagesOnboardingthree
+  /// assets/images/onboardingthree.PNG
+  static const String imagesOnboardingthree = "assets/images/onboardingthree.PNG";
+
+  /// Assets for imagesOnboardingtwo
+  /// assets/images/onboardingtwo.PNG
+  static const String imagesOnboardingtwo = "assets/images/onboardingtwo.PNG";
+}
+
