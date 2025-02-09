d9967878e19c8cead0599e01038a168e4f435e27
King <jafaar.abdulkareem@gmail.com>
2025-02-09 22:17:26 +0530

    create app color for application

---
+++ lib/core/constant/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  // static const Color
+}
+
+abstract class AppColorText{
+  static const Color primary = Color(0xFF000000);
+  static const Color secondary = Color(0xFF8B8787);
+}
