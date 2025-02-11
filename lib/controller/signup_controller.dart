4d1c7c5cde697c79acfe37cc5c86ee97299f1239
King <jafaar.abdulkareem@gmail.com>
2025-02-11 15:38:56 +0530

    created controller and view screen for sign up

---
+++ lib/controller/signup_controller.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+abstract class SignupController extends GetxController {
+  void linkOnTap();
+  void signupOnTap();
+}
+
+class SignupControllerImp extends SignupController {
+  late TextEditingController username;
+  late TextEditingController email;
+  late TextEditingController password;
+  late TextEditingController phone;
+  @override
+  void onInit() {
+    username = TextEditingController();
+    email = TextEditingController();
+    password = TextEditingController();
+    phone = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    username.dispose();
+    email.dispose();
+    password.dispose();
+    phone.dispose();
+    super.dispose();
+  }
+
+  @override
+  void linkOnTap() {
+    Get.back();
+  }
+
+  @override
+  void signupOnTap() {}
+}
---
+++ lib/view/screen/auth/signup_view.dart
@@ -0,0 +1,81 @@
+import 'package:ecommerce/controller/signup_controller.dart';
+import 'package:ecommerce/core/constant/app_color.dart';
+import 'package:ecommerce/core/constant/app_icon.dart';
+import 'package:ecommerce/core/constant/app_style.dart';
+import 'package:ecommerce/core/localization/key_language.dart';
+import 'package:ecommerce/core/share/custom_button_widget.dart';
+import 'package:ecommerce/core/share/custom_text_form_field_widget.dart';
+import 'package:ecommerce/view/widget/login/link_message.dart';
+import 'package:ecommerce/view/widget/login/title_description_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignupView extends StatelessWidget {
+  const SignupView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final SignupControllerImp controller = Get.put(SignupControllerImp());
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          KeyLanguage.signup.tr,
+          style: AppStyle.styleBold18(context),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const TitleDescriptionWidget(),
+              CustomTextFormFieldWidget(
+                hint: KeyLanguage.usernameHint.tr,
+                label: KeyLanguage.usernameLabel.tr,
+                icon: AppIcon.username,
+                controller: controller.username,
+              ),
+              CustomTextFormFieldWidget(
+                hint: KeyLanguage.emailHint.tr,
+                label: KeyLanguage.emailLabel.tr,
+                icon: AppIcon.email,
+                controller: controller.email,
+              ),
+              CustomTextFormFieldWidget(
+                hint: KeyLanguage.passwordHint.tr,
+                label: KeyLanguage.passwordLabel.tr,
+                icon: AppIcon.password,
+                controller: controller.password,
+                obscure: true,
+              ),
+              CustomTextFormFieldWidget(
+                hint: KeyLanguage.phoneHint.tr,
+                label: KeyLanguage.phoneLabel.tr,
+                icon: AppIcon.phone,
+                controller: controller.phone,
+              ),
+              CustomButtonWidget(
+                text: KeyLanguage.signup.tr,
+                color: AppColor.primary,
+                onTap: () {
+                  controller.signupOnTap();
+                },
+              ),
+              LinkMessage(
+                message: KeyLanguage.messageLinkSignup.tr,
+                link: KeyLanguage.login.tr,
+                onTap: () {
+                  controller.linkOnTap();
+                },
+              ),
+              const SizedBox(height: 32),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
