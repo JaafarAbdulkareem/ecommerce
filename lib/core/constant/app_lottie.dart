077d28aa6572eec737f845d7fbc9e80917ca6f45
King <jafaar.abdulkareem@gmail.com>
2025-02-16 17:58:31 +0530

    insert lottie image

---
+++ lib/core/constant/app_lottie.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  AppImages._();
+  static const String _path = "assets/lottie";
+  static const String cart = "$_path/cart.json";
+  static const String ecommerce = "$_path/ecommerce.json";
+  static const String nodata = "$_path/nodata.json";
+  static const String offline = "$_path/offline.json";
+  static const String server = "$_path/server.json";
+}
