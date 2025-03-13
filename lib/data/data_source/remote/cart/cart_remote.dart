5d151cca052bd8b41e289b6b8cc772da7b4afcac
King <jafaar.abdulkareem@gmail.com>
2025-03-13 16:56:19 +0530

    create remote for cart

---
+++ lib/data/data_source/remote/cart/cart_remote.dart
@@ -0,0 +1,49 @@
+import 'package:ecommerce/core/constant/api_constant.dart';
+import 'package:ecommerce/core/constant/api_key.dart';
+import 'package:ecommerce/core/service/curd.dart';
+
+class CartRemote {
+  final Curd curd;
+
+  CartRemote({required this.curd});
+
+  getData({
+    required String userId,
+  }) async {
+    var response = await curd.postData(ApiConstant.apiView, {
+      ApiKey.userId: userId,
+    });
+    return response.fold((left) => left, (right) => right);
+  }
+
+  getDeleteData({
+    required String id,
+  }) async {
+    var response = await curd.postData(ApiConstant.apiDeleteAllProduct, {
+      ApiKey.userId: id,
+    });
+    return response.fold((left) => left, (right) => right);
+  }
+
+  getIncrementData({
+    required String userId,
+    required String productId,
+  }) async {
+    var response = await curd.postData(ApiConstant.apiIncrement, {
+      ApiKey.userId: userId,
+      ApiKey.productId: productId,
+    });
+    return response.fold((left) => left, (right) => right);
+  }
+
+  getDecrementData({
+    required String userId,
+    required String productId,
+  }) async {
+    var response = await curd.postData(ApiConstant.apiIncrement, {
+      ApiKey.userId: userId,
+      ApiKey.productId: productId,
+    });
+    return response.fold((left) => left, (right) => right);
+  }
+}
