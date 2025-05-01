b2c3c4ad801ee1760154881cb0f0f518d60495cc
King <jafaar.abdulkareem@gmail.com>
2025-05-01 15:48:12 +0530

    update home and delete auth model

--- lib/controller/home/home_controller.dart
+++ lib/controller/home/home_controller.dart
@@ -1,33 +1,24 @@
 import 'package:ecommerce/core/class/status_request.dart';
-import 'package:ecommerce/core/constant/api_key.dart';
+import 'package:ecommerce/core/constant/constant_key.dart';
+import 'package:ecommerce/core/constant/constant_scale.dart';
 import 'package:ecommerce/core/service/shared_prefs_service.dart';
-import 'package:ecommerce/data/models/auth_model.dart';
 import 'package:get/get.dart';
 
 abstract class HomeController extends GetxController {
-  void sharedPreferenceInitial();
   void changeBottonBar(int currentnIdex);
 }
 
 class HomeControllerImp extends HomeController {
   late SharedPrefsService sharedPrefsService;
-  late AuthModel authData;
+  late String username;
   late int indexBar;
   late StatusRequest statusRequest;
-  @override
-  void sharedPreferenceInitial() async {
-    authData = AuthModel(
-      username: sharedPrefsService.prefs.getString(ApiKey.username) ?? "",
-      email: sharedPrefsService.prefs.getString(ApiKey.email)!,
-      phone: sharedPrefsService.prefs.getString(ApiKey.phone)!,
-    );
-  }
 
   @override
   void onInit() {
-    sharedPrefsService = sharedPrefsService = Get.find<SharedPrefsService>();
-    sharedPreferenceInitial();
-    indexBar = 0;
+    sharedPrefsService = Get.find<SharedPrefsService>();
+    username = sharedPrefsService.prefs.getString(ConstantKey.keyUsername)!;
+    indexBar = ConstantScale.defalutIndexView;
     statusRequest = StatusRequest.initial;
     super.onInit();
   }
