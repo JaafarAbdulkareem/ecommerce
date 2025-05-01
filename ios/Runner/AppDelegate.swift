d1ec1017e1412cd387251844342084993f2e17f4
King <jafaar.abdulkareem@gmail.com>
2025-05-01 16:38:33 +0530

    initial  for google map and install google map flutter and location packages

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDTD1rSTzki9zq6PpMQAys1JTq1GkxpQB8")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
