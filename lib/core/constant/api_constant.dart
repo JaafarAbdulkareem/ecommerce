138e193dd32c4339bbb96f8995fc2c3049ad2214
King <jafaar.abdulkareem@gmail.com>
2025-06-05 09:50:43 +0530

    insert user folder because i will insert derivery

--- lib/core/constant/api_constant.dart
+++ lib/core/constant/api_constant.dart
@@ -1,6 +1,6 @@
 abstract class ApiConstant {
   static const String server =
-      "https://3e01-49-206-5-119.ngrok-free.app/"; // ngrok ngrok http --url=pleasantly-divine-bear.ngrok-free.app 80
+      "https://46ba-49-206-0-60.ngrok-free.app/"; // ngrok ngrok http --url=pleasantly-divine-bear.ngrok-free.app 80
   // static const String server = "http://192.168.0.113"; // home wifi
   // static const String server = "http://192.168.230.139";
   // static const String server = "http://192.168.50.139"; // mobile
@@ -10,8 +10,8 @@ abstract class ApiConstant {
   static const String imagePath = "$serverPath/upload";
   static const String categoryImagePath = "$imagePath/category";
   static const String productImagePath = "$imagePath/product";
-
-  static const String folderAuth = "$serverPath/auth";
+ static const String folderUser = "$serverPath/users/";
+  static const String folderAuth = "$folderUser/auth";
   static const String apiSignup = "$folderAuth/signup.php";
   static const String apiVerificationSignup =
       "$folderAuth/verify_code_signup.php";
@@ -21,11 +21,11 @@ abstract class ApiConstant {
   static const String apiResetPassword = "$folderAuth/reset_password.php";
   static const String apiViewAuth = "$folderAuth/view_auth.php";
 
-  static const String folderHome = "$serverPath/home";
+  static const String folderHome = "$folderUser/home";
   static const String apiHome = "$folderHome/home.php";
   static const String apiSearchPrdouct = "$folderHome/search_product.php";
 
-  static const String folderCart = "$serverPath/cart";
+  static const String folderCart = "$folderUser/cart";
   static const String apiView = "$folderCart/view.php";
   static const String apiInsert = "$folderCart/insert.php";
   static const String apiDeleteAllProduct =
@@ -33,16 +33,16 @@ abstract class ApiConstant {
   static const String apiIncrement = "$folderCart/increment.php";
   static const String apiDecrement = "$folderCart/decrement.php";
 
-  static const String folderAddress = "$serverPath/address";
+  static const String folderAddress = "$folderUser/address";
   static const String apiViewAddress = "$folderAddress/view_address.php";
   static const String apiInsertAddress = "$folderAddress/insert_address.php";
   static const String apiDeleteAddress = "$folderAddress/delete_address.php";
   static const String apiUpdateAddress = "$folderAddress/update_address.php";
 
-  static const String folderCoupons = "$serverPath/coupons";
+  static const String folderCoupons = "$folderUser/coupons";
   static const String apiViewCoupons = "$folderCoupons/view_coupons.php";
 
-  static const String folderOrder = "$serverPath/order";
+  static const String folderOrder = "$folderUser/order";
   // static const String apiCheckProductNo =
   //     "$folderOrder/check_product_no.php";
   static const String apiCheckout = "$folderOrder/checkout.php";
@@ -50,8 +50,12 @@ abstract class ApiConstant {
   static const String apiDeleteOrder = "$folderOrder/delete_order.php";
   static const String apiViewArchive = "$folderOrder/view_archive.php";
   static const String apiViewDetailOrder = "$folderOrder/view_detail_order.php";
+  static const String apiViewInvalidOrder =
+      "$folderOrder/view_invalid_order.php";
+  static const String apiInsertSubmitInvalidOrder =
+      "$folderOrder/insert_submit_invalid_order.php";
 
-  static const String folderNotification = "$serverPath/notification";
+  static const String folderNotification = "$folderUser/notification";
   static const String apiViewNotification =
       "$folderNotification/view_notification.php";
   static const String apiInsertNotification =
@@ -59,6 +63,6 @@ abstract class ApiConstant {
   static const String apiDeleteNotification =
       "$folderNotification/delete_notification.php";
 
-  static const String folderRating = "$serverPath/rating";
+  static const String folderRating = "$folderUser/rating";
   static const String apiInsertRating = "$folderRating/insert_rating.php";
 }
